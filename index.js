2709dc68275f0feea08ec9caec74df8fb977351e
Erik Alves de Matos <erik.matos@MacBook-Pro-de-Erik.local>
2016-06-24 14:37:07 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let fs = require('fs')
+let restify = require('restify')
+
+
+function watcher(req, res, next) {
+    //res.send('hello ' + req.params);
+
+    console.log(req.params)
+    res.send()
+    next();
+}
+
+let server = restify.createServer()
+
+server.get('/1/trello/webhook', watcher)
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url)
+});
