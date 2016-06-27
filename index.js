f7b1c1ba8ec631ffcaa2a7dd5283f1cea2aa2377
erik.matos <erik.matos@oulook.com>
2016-06-27 12:40:06 -0300

    version adjust

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let restify = require('restify')
 function watcher(req, res, next) {
     //res.send('hello ' + req.params);
 
-    console.log(req.params)
+    console.log(req)
     res.send()
     next();
 }
@@ -15,6 +15,8 @@ function watcher(req, res, next) {
 let server = restify.createServer()
 
 server.get('/1/trello/webhook', watcher)
+
+server.post('/1/trello/webhook', watcher)
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url)
