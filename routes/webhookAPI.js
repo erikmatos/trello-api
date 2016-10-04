ced99c5aa25b550300572fea22927a62a73b4c7d
erikmatos <drik0905>
2016-10-04 10:57:20 -0300

    log

--- routes/webhookAPI.js
+++ routes/webhookAPI.js
@@ -32,7 +32,6 @@ class WebhookAPI {
         });
 
         server.head('/webhook', (req, res, next)=> {
-            console.log('XPTO');
             res.send(200);
             next()
         })
