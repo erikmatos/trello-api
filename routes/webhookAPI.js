f8d6d45ee808224d3088a50af303aaa53d7ff310
Erik Alves de Matos <erik.matos@emanagecrm.com>
2017-04-18 12:10:52 -0300

    development

--- routes/webhookAPI.js
+++ routes/webhookAPI.js
@@ -19,6 +19,23 @@ class WebhookAPI {
             this.logger.info(payload);
             this.logger.info(req.params.id);
             this.logger.info(referrer);
+            this.logger.info('POST');
+
+            res.send(200);
+
+            next();
+        });
+
+        server.get('/webhook/:id', (req, res, next)=> {
+
+            let referrer = req.headers['x-forwarded-for'];
+
+            let payload = req.body;
+
+            this.logger.info(payload);
+            this.logger.info(req.params.id);
+            this.logger.info(referrer);
+            this.logger.info('GET');
 
             res.send(200);
 
@@ -30,6 +47,7 @@ class WebhookAPI {
             let remote = req.headers['x-forwarded-for'];
 
             this.logger.info(remote);
+            this.logger.info('HEAD');
 
             res.send(200);
             next()
