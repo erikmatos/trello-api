f0a8c74c9a65e0b7b088b1a350acc64481792159
Erik Alves de Matos <erik.matos@emanagecrm.com>
2017-05-15 13:57:55 -0300

    development

--- services/notification.js
+++ services/notification.js
@@ -13,21 +13,28 @@ class NotificationService {
 
     notify(payload) {
 
-        var sender = new gcm.Sender('AIzaSyCVPK-NxED8LiXpAmrJ812I4Gs7NusEmhM');
-
+        //sender
+        let sender = new gcm.Sender('AIzaSyCVPK-NxED8LiXpAmrJ812I4Gs7NusEmhM');
 
         // Prepare a message to be sent
-        var message = new gcm.Message({
-            data: { key1: 'msg1' }
-        });
+        let message = new gcm.Message({data: { key1: 'msg1' }});
 
         // Specify which registration IDs to deliver the message to
-        var regTokens = ['APA91bGamkoBIanpM3Otb58mEsMsU06dh-DYkKJIQprCmwbajotRX7wGk63sGe_lZsQ1LvyyMZOOGoWCPRasVu0IybIKUFc9ly5TLdZZbBJPHcMB5oDBY-dLnGgbfIiVFhmW5mupp6g4'];
+        let regTokens = ['dbZimjABRyY:APA91bGamkoBIanpM3Otb58mEsMsU06dh-DYkKJIQprCmwbajotRX7wGk63sGe_lZsQ1LvyyMZOOGoWCPRasVu0IybIKUFc9ly5TLdZZbBJPHcMB5oDBY-dLnGgbfIiVFhmW5mupp6g4'];
 
         // Actually send the message
         sender.send(message, { registrationTokens: regTokens }, function (err, response) {
-            if (err) console.error(err);
-            else console.log(response);
+
+            if (err) {
+
+                console.error(err);
+
+            } else {
+
+                console.log(response);
+
+            }
+
         });
 
     }
