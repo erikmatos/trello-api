f636c076f486d4d655e24de990660ebe32ea0273
Erik Alves de Matos <erik.matos@emanagecrm.com>
2017-05-15 15:14:50 -0300

    development

--- services/notification.js
+++ services/notification.js
@@ -13,14 +13,16 @@ class NotificationService {
 
     notify(payload) {
 
+        let requestOptions = { proxy: 'http://127.0.0.1:8000', timeout: 5000 };
+
         //sender
-        let sender = new gcm.Sender('AIzaSyAASEsaRIz8KT2Z-rK__1fJP4rglhe7jJM');
+        let sender = new gcm.Sender('AIzaSyASQIxJNtC_lTKIevfzhQCw7FsyHQm6tz4');
 
         // Prepare a message to be sent
         let message = new gcm.Message({notification: { from: 'Erik', message: 'Hello Dear!' }});
 
         // Specify which registration IDs to deliver the message to
-        let regTokens = ['APA91bGamkoBIanpM3Otb58mEsMsU06dh-DYkKJIQprCmwbajotRX7wGk63sGe_lZsQ1LvyyMZOOGoWCPRasVu0IybIKUFc9ly5TLdZZbBJPHcMB5oDBY-dLnGgbfIiVFhmW5mupp6g4'];
+        let regTokens = ['dbZimjABRyY:APA91bGamkoBIanpM3Otb58mEsMsU06dh-DYkKJIQprCmwbajotRX7wGk63sGe_lZsQ1LvyyMZOOGoWCPRasVu0IybIKUFc9ly5TLdZZbBJPHcMB5oDBY-dLnGgbfIiVFhmW5mupp6g4'];
 
         // Actually send the message
         sender.send(message, { registrationTokens: regTokens }, function (err, response) {
